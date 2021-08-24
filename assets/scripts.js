df21ed20d82387d1e1e871a03b3292f182ea1eb8
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-08-24 12:56:40 -0600

    added id of past present futrue

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
+    var currentFormattedTime = moment().format('MMMM D, YYYY, dddd h:mm:ss a');
+    $("#currentDay").text(currentFormattedTime);
+    
+    var goldenTime = moment().hour();
+    $(".time-block").each(function(index,timeBlock) {
+        var hour = parseInt($(timeBlock).attr("hour"));
+        if (hour < goldenTime) {
+            $(timeBlock).addClass("past");
+        } else if (hour === goldenTime) {
+            $(timeBlock).addClass("present");
+        }else if (hour > goldenTime) {
+            $(timeBlock).addClass("future");
+        }
+    });
+
     
    
 });
